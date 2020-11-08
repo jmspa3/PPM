5453cff2586018176f03aac0b4e0491eec102e72
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-11-08 16:49:13 +0000

    Task and Dependencies

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "ppm"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
+libraryDependencies ++= Seq(
+  "joda-time:joda-time:2.9.9",
+  "org.joda:joda-convert:1.2" ,
+  "com.github.nscala-time:nscala-time:2.24.0"
+)
---
+++ src/main/scala/Task.scala
@@ -0,0 +1,56 @@
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import android.renderscript.RenderScript.Priority
+import com.github.nscala_time.time.Imports._
+import org.joda.time.Days
+
+case class User(id : Int)
+
+case class Task(id: Int, name : String, creationDate: DateTime, deadline : DateTime, done : Boolean ,priority: String) {
+  def daysLeft(): Unit = Task.daysLeft(this)
+  def setDone(): Task = Task.setDone(this)
+  def isDone(): Boolean = Task.isDone(this)
+  def getPriority(): String = Task.getPriority(this)
+  def editName(newName : String): Task = Task.editName(this,newName)
+
+}
+
+object Task{
+
+  type ID = Int
+  type name = String
+  type description = String
+  type creationDate = DateTime
+  type deadline = DateTime
+  type done = Boolean
+  type priority = String
+  type Members = List[User]
+
+  // 2: get a date to represent Christmas
+
+def editName(t:Task, newName:String): Task  = {
+  Task(t.id,newName,t.creationDate,t.deadline,t.done,t.priority)
+}
+  def daysLeft(t:Task): Unit = {
+    val daysToDeadline = Days.daysBetween(t.creationDate, t.deadline).getDays
+    println(daysToDeadline)
+  }
+
+  def isDone(t:Task): Boolean = {
+    t.done
+  }
+
+  def getPriority(t:Task): String = {
+    t.priority
+  }
+
+  def setDone(t:Task): Task = {
+    Task(t.id,t.name,t.creationDate,t.deadline,true,t.priority)
+  }
+
+
+
+
+
+}
