8ff6f25bd419f26880de1b37b999177616da474a
simoesd <dfmss2@iscte.pt>
2020-11-10 15:31:05 +0000

    Update

--- PPMProject/src/Main.scala
+++ PPMProject/src/Main.scala
@@ -1,23 +1,72 @@
-
 import Task.isDone
 import com.github.nscala_time.time.Imports.DateTime
 import org.joda.time.Days
 import org.joda.time.Days
 
 import scala.annotation.tailrec
+import scala.io.StdIn.readLine
 
 object Main {
+   sharedFileMenu()
 
-  def main(args: Array[String]): Unit = {
-    val deadline = (new DateTime).withYear(2020)
-      .withMonthOfYear(11)
-      .withDayOfMonth(6)
+   def main(args: Array[String]): Unit = {
+      val deadline = (new DateTime).withYear(2020)
+         .withMonthOfYear(11)
+         .withDayOfMonth(6)
 
-    val task: Task = Task(1,"nome",DateTime.now(),deadline,false,"high")
-    task.daysLeft()
-    val taskDone : Task = task.setDone()
-    println(taskDone.name)
+      val task: Task = Task(1,"nome",DateTime.now(),deadline,false,"high")
+      task.daysLeft()
+      val taskDone : Task = task.setDone()
+      println(taskDone.name)
 
-  }
+   }
+
+   def sharedFileMenu() = {
+      val databasePath = "savedFiles"
+      mainLoopFileMenu()
+      def mainLoopFileMenu():Any =
+      {
+         println("SharedFile Menu:\n")
+         println("1. Check uploaded files")
+         println("2. Upload new File")
+         println("3. Discard File")
+         println("0. Exit program")
+         val userChoice = readLine.trim
+         userChoice match
+         {
+            case "0" => println("Exiting...")
+            case "1" =>
+            {
+               val savedFiles = StorageManager.readObjectFromFile(databasePath)
+               savedFiles.asInstanceOf[List[SharedFile]] map (x => println(x))
+               mainLoopFileMenu()
+            }
+            case "2" =>
+            {
+               println("Insert file path: ")
+               val newFilePath = readLine().trim
+               val newFileName = newFilePath.split("/").last
+               val savedFiles = StorageManager.readObjectFromFile(databasePath).asInstanceOf[List[SharedFile]]
+               val sh = new SharedFile(newFileName, {if (savedFiles.length > 0) savedFiles.last.getId() + 1 else 0}, newFilePath, List())
+               StorageManager.writeObjectInFile(sh, databasePath, append = true)
+               mainLoopFileMenu()
+            }
+            case "3" =>
+            {
+               val savedFiles = StorageManager.readObjectFromFile(databasePath)
+               savedFiles.asInstanceOf[List[SharedFile]] map (x => println(x))
+               println("Insert ID of file to delete: ")
+               val fileId = readLine().trim.toInt
+               val newSavedFiles = savedFiles.asInstanceOf[List[SharedFile]] filter (x => x.getId() != fileId)
+               StorageManager.writeObjectListInFile(newSavedFiles, databasePath, append = false)
+               mainLoopFileMenu()
+            }
+            case _ => {
+               println("Invalid choice!")
+               mainLoopFileMenu()
+            }
+         }
+      }
+   }
 
 }
---
+++ PPMProject/src/SharedFile.scala
@@ -0,0 +1,53 @@
+import java.io.Serializable
+import java.util.Date
+
+case class Comment(user: User, date: Date, content: String)
+
+case class SharedFile(fileName: String, id: Int, path: String, comments: List[Comment]) extends Serializable {
+   def getName() = SharedFile.getName(this)
+   def getId() = SharedFile.getId(this)
+   def getPath() = SharedFile.getPath(this)
+   def getComments() = SharedFile.getComments(this)
+   def addComment(comment: Comment) = SharedFile.addComment(this, comment)
+   override def toString() = SharedFile.toString(this)
+
+}
+
+
+object SharedFile {
+
+   type fileName = String
+   type id = Int
+   type path = String
+   type comments = List[Comment]
+
+   def getName(sh: SharedFile) =
+   {
+      sh.fileName
+   }
+
+   def getId(sh: SharedFile) =
+   {
+      sh.id
+   }
+
+   def getPath(sh: SharedFile) =
+   {
+      sh.path
+   }
+
+   def getComments(sh: SharedFile) = {
+      sh.comments
+   }
+
+   def addComment(sh: SharedFile, comment: Comment): SharedFile =
+   {
+      new SharedFile(sh.fileName, sh.id, sh.path, sh.comments ++ List(comment))
+   }
+
+   def toString(sh: SharedFile): String =
+   {
+      sh.id + ": " + sh.fileName
+   }
+
+}
