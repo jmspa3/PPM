9f9963bd896d7b3c161631ebb412ee45ed79b22c
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-11-20 18:00:08 +0000

    Update Task.scala

--- PPMProject/src/Task.scala
+++ PPMProject/src/Task.scala
@@ -1,16 +1,22 @@
 package PPMProject
+import java.text.SimpleDateFormat
+import java.util.Calendar
 import com.github.nscala_time.time.Imports._
 import org.joda.time.Days
 
+case class User(id : Int)
 
-case class Task(id: Int, name : String, creationDate: DateTime, deadline : DateTime, done : Boolean ,priority: String) {
+case class Task(id: Int, name : String, creationDate: DateTime, done : Boolean ,priority: String) extends Serializable {
   def daysLeft(): Unit = Task.daysLeft(this)
   def setDone(): Task = Task.setDone(this)
   def isDone(): Boolean = Task.isDone(this)
+  def getId(): Int = Task.getID(this)
+  def getName(): String = Task.getName(this)
   def getPriority(): String = Task.getPriority(this)
   def editName(newName : String): Task = Task.editName(this,newName)
 
 }
+
 
 object Task{
 
@@ -26,10 +32,13 @@ object Task{
   // 2: get a date to represent Christmas
 
   def editName(t:Task, newName:String): Task  = {
-    Task(t.id,newName,t.creationDate,t.deadline,t.done,t.priority)
+    Task(t.id,newName,t.creationDate,t.done,t.priority)
   }
   def daysLeft(t:Task): Unit = {
-    val daysToDeadline = Days.daysBetween(t.creationDate, t.deadline).getDays
+    val deadline = (new DateTime).withYear(2020)
+      .withMonthOfYear(11)
+      .withDayOfMonth(6)
+    val daysToDeadline = Days.daysBetween(t.creationDate, deadline).getDays
     println(daysToDeadline)
   }
 
@@ -37,12 +46,18 @@ object Task{
     t.done
   }
 
+  def getID(t:Task): Int = {
+    t.id
+  }
+  def getName(t:Task): String = {
+    t.name
+  }
   def getPriority(t:Task): String = {
     t.priority
   }
 
   def setDone(t:Task): Task = {
-    Task(t.id,t.name,t.creationDate,t.deadline,true,t.priority)
+    Task(t.id,t.name,t.creationDate,true,t.priority)
   }
 
 
