a850ea3b9b4be48ea602aa9519737fa8ee3b3d4a
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2020-12-09 18:08:11 +0000

    Update Ze

---
+++ PPMProject/src/UI/Login/LoginController.scala
@@ -0,0 +1,26 @@
+package UI.Login
+
+import java.util
+
+import UI.Project
+import javafx.collections.FXCollections
+import javafx.fxml.{FXML, FXMLLoader}
+import javafx.scene.Parent
+import javafx.scene.control.{Button, ListView, TextField}
+class LoginController {
+  @FXML
+  private var button1: Button = _
+  @FXML
+  private var textField1: TextField = _
+  def onButton1Clicked(): Unit = {
+    println("Hello World")
+    val fxmlLoader = new FXMLLoader(getClass.getResource("../Menu/MenuController.fxml"))
+    val mainViewRoot: Parent = fxmlLoader.load()
+    button1.getScene().setRoot(mainViewRoot)
+
+
+
+  }
+
+
+}
---
+++ PPMProject/src/UI/Menu/MenuController.scala
@@ -0,0 +1,45 @@
+package UI.Menu
+
+import java.util
+
+import PPMProject.Task
+import UI.FxApp
+import javafx.fxml.{FXML, FXMLLoader}
+import javafx.scene.Parent
+import javafx.scene.control.{Button, Label, ListView, ScrollPane, TextField}
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
+
+import scala.::
+
+class MenuController {
+  @FXML
+  private var button1: Button = _
+  @FXML
+  private var textField1: TextField = _
+  @FXML
+  private var textField2: TextField = _
+  @FXML
+  private var scroll: ScrollPane= _
+  @FXML
+  private var listView1: ListView[HBox] = _
+  var vbox = new VBox(8) // spacing = 8
+
+
+  def onButton1Clicked(): Unit = {
+    
+    vbox.getChildren.addAll(new HBox(new Label("Task1"), new Button("Delete")))
+  }
+
+  @FXML def initialize(): Unit = {
+
+    vbox.getChildren.addAll(new HBox(new Label("Task1"), new Button("Delete")))
+    vbox.getChildren.add(new HBox(new Label("Task1"), new Button("Delete")))
+    vbox.getChildren.add(new HBox(new Label("Task1"), new Button("Delete")))
+    scroll.setContent(vbox)
+  }
+
+  FxApp.task
+}
